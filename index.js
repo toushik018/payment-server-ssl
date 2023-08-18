db687075c0db5b2f2716df1e371f15d0ce3f7ea1
Toushik <tayebhossain018@gmail.com>
2023-08-18 19:58:15 +0600

    updated

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ async function run() {
                 total_amount: order.price,
                 currency: 'BDT',
                 tran_id: tran_id, // use unique tran_id for each api call
-                success_url: `http://localhost:5000/payment/success/${tran_id}`,
-                fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
+                success_url: `https://payment-server-ssl-production.up.railway.app/payment/success/${tran_id}`,
+                fail_url: `https://payment-server-ssl-production.up.railway.app/fail/${tran_id}`,
                 cancel_url: 'http://localhost:3030/cancel',
                 ipn_url: 'http://localhost:3030/ipn',
                 shipping_method: 'Courier',
